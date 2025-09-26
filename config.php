b12c9cabbd6fd247fb8f56d29fcf053c1c21eb3b
Naveen <naveensrinivasan112@gmail.com>
2025-09-26 18:19:54 +0530

    successfully commited

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+define('_AMSCODESECURITY', '16343942');
+define('CURRENCY', '$');
+define('WEB_URL', 'http://localhost/Responsive_Apartment/');
+define('ROOT_PATH', dirname(__FILE__)."/");
+
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'Responsive_Apartment');
+$link = new mysqli(DB_HOSTNAME,DB_USERNAME,DB_PASSWORD,DB_DATABASE);?>
