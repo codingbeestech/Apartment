54c80e2c5a359869fc1ac55899f2cf10a93039de
Sudhakar-LF <sudhakar@lfsolutions.net>
2025-10-30 11:41:12 +0530

    Upgrade the Version and successfully run

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 define('_AMSCODESECURITY', '16343942');
 define('CURRENCY', '$');
-define('WEB_URL', 'http://localhost/Responsive_Apartment/');
+define('WEB_URL', 'https://apartment.codingbees.tech/');
 define('ROOT_PATH', dirname(__FILE__)."/");
 
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'Responsive_Apartment');
+define('DB_USERNAME', 'u866818024_apartment');
+define('DB_PASSWORD', 'CodingBees@123');
+define('DB_DATABASE', 'u866818024_apartment_db');
 $link = new mysqli(DB_HOSTNAME,DB_USERNAME,DB_PASSWORD,DB_DATABASE);?>
